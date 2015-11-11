695bb4c21b7949a043ccd5c3ab5eb97ef6648dac
Ronald Osure <sureronald@gmail.com>
2015-11-11 16:55:54 +0300

    Introduce Config Parser and sanitize code

--- crlpublisher.py
+++ crlpublisher.py
@@ -8,10 +8,18 @@ Author: Ronald Osure
 import sys
 import datetime
 import os
+import ConfigParser
+
+CONFIG_FILE = '/etc/crlpublisher/crlpublisher.conf'
 
 def main():
-  os.system("cp "+sys.argv[1]+" /tmp/crlpublisher.tmp")
-  f = open('/var/log/crlpublisher/crlpublisher.log', 'a')
+  config = ConfigParser.RawConfigParser()
+  config.read(CONFIG_FILE)
+
+  os.system("scp -i %s %s %s@%s:%s" % (config.get('local','identityFile'), sys.argv[1], config.get('remote','remoteUser'),config.get('remote','remoteHost'),config.get('remote','remoteLocation')))
+
+  #Take down some logs
+  f = open(config.get('local','logFile'), 'a')
   data = str(datetime.datetime.now()) + ' - ' + str(sys.argv) + '\n'
   f.write(data)
   f.close()
