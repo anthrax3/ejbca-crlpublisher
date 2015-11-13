c60c5743da04d729ee7be88de28ff1141b7b657e
Ronald Osure <sureronald@gmail.com>
2015-11-13 16:56:43 +0300

    Add delete old CRL file before copy + minor fixes

--- crlpublisher.py
+++ crlpublisher.py
@@ -16,9 +16,15 @@ def main():
   config = ConfigParser.RawConfigParser()
   config.read(CONFIG_FILE)
 
+  # Delete the old CRL file. The reason for manual delete is because scp doesn't overwrite a file when you specify the target file name in the scp commnad
+
+  os.system("ssh -i %s %s@%s rm %s" % (config.get('local','identityFile'), config.get('remote','remoteUser'),config.get('remote','remoteHost'),config.get('remote','remoteLocation')))
+
+  # We can now copy the file over
+
   os.system("scp -i %s %s %s@%s:%s" % (config.get('local','identityFile'), sys.argv[1], config.get('remote','remoteUser'),config.get('remote','remoteHost'),config.get('remote','remoteLocation')))
 
-  #Take down some logs
+  #Log event
   f = open(config.get('local','logFile'), 'a')
   data = str(datetime.datetime.now()) + ' - ' + str(sys.argv) + '\n'
   f.write(data)
